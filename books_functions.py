e2bd1469d29257cebc04d9880833f5ff9698a97b
Milla Kelhu <milla.kelhu@gmail.com>
2021-05-18 11:26:00 +0300

    Debug

--- books_functions.py
+++ books_functions.py
@@ -1,7 +1,7 @@
 from db import db
 
 def get_all_books():
-    sql = "SELECT b.id, b.title, a.name AS author FROM books b, authors a WHERE b.author_id=a.id"
+    sql = "SELECT b.id, b.title, a.name FROM books b, authors a WHERE b.author_id=a.id"
     return db.session.execute(sql).fetchall()
 
 def add_books():
--- routes.py
+++ routes.py
@@ -1,7 +1,7 @@
 from app import app
 from flask import render_template
-import books, users
+import books_functions
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    return render_template("index.html", books=books_functions.get_all_books())
